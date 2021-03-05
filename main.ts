d664e448ef552bc31702bd6edb4db874406783d3
novomestan <78145236+novomestan@users.noreply.github.com>
2021-03-05 20:13:32 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 let strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
-strip.setBrightness(128)
 basic.forever(function () {
     strip.show()
     strip.rotate(1)
