4ea02fe0978f11f25be4e5652b36ed01842e9e53
novomestan <78145236+novomestan@users.noreply.github.com>
2021-03-05 19:56:23 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
+strip.showRainbow(1, 360)
 basic.forever(function () {
-	
+    strip.show()
+    strip.showRainbow(1, 360)
+    basic.pause(500)
 })
